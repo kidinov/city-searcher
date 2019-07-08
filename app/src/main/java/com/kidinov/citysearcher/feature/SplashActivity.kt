b56e2be33992151fb8a58941435056636e10f33c
andrey <mGoogleLocationListener>
2019-07-08 10:24:48 +0200

    Project init

---
+++ app/src/main/java/com/kidinov/citysearcher/feature/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.kidinov.citysearcher.feature
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kidinov.citysearcher.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
